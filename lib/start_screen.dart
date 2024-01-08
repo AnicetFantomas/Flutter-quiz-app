a5400d49a4a680886e0576c09079d91cf5d7c6d5
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-08 12:24:45 +0200

    Setup project

---
+++ lib/start_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+
+  Widget build(context) {
+     return const Center(child: Column(children: [
+      
+     ],)
+     );
+  }
+}

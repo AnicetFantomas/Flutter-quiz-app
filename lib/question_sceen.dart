a83554aef2decf3f47d5b2d9fc4b02ad79c5e8a8
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-08 14:02:34 +0200

    Add Icons

---
+++ lib/question_sceen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key});
+  @override
+  State<QuestionScreen> createState() {
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  @override
+  Widget build(context) {
+    return const Text('Question Screen');
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,19 +9,26 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+        Image.asset('assets/images/quiz-logo.png', width: 300, color: const Color.fromARGB(116, 250, 249, 248),),
+          // Opacity(
+          //     opacity: 0.2,
+          //     child: Image.asset('assets/images/quiz-logo.png', width: 300)),
           const SizedBox(height: 80),
           const Text('Learn Flutter the fun way!',
               style: TextStyle(fontSize: 24, color: Colors.white)),
           const SizedBox(height: 30),
-          OutlinedButton.icon(onPressed: () {},
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.white,  
-          ),
-          icon: const Icon(Icons.arrow_right_alt),
-           label: const Text('Start Quiz'),)
+          OutlinedButton.icon(
+            onPressed: () {
+              
+            },
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
         ],
-      ), 
+      ),
     );
   }
 }

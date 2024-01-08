f918593403456bf4837a320d303ef9bb53e8c9f7
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-08 13:04:20 +0200

    Complete the start screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ void main() {
                   Color.fromARGB(255, 78, 13, 151),
                   Color.fromARGB(255, 107, 15, 168),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: const StartScreen(),),
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,11 +4,23 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
-
   Widget build(context) {
-     return const Center(child: Column(children: [
-      
-     ],)
-     );
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text('Learn Flutter the fun way!',
+              style: TextStyle(fontSize: 24, color: Colors.white)),
+          const SizedBox(height: 30),
+          OutlinedButton(onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.white,  
+          ),
+           child: const Text('Start Quiz'),)
+        ],
+      ),
+    );
   }
 }

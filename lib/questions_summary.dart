8f034dc028c873c4e26ee6becdfcb102b018c57a
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-12 17:39:07 +0200

    Add question summary displayed

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -6,22 +6,27 @@ class QuestionSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
   @override
   Widget build(context) {
-    return Column(
-      children: summaryData.map((data) {
-        return  Row(children: [
-          Text(((data['question_index'] as int) + 1).toString()),
-          Expanded(
-            child: Column(
-              children: [
-                Text(data['question'] as String),
-                const SizedBox(height: 5),
-                Text(data['user_answer'] as String),
-                Text(data['correct_answer'] as String),
-              ],
-            ),
-          )
-        ],);
-      },).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return  Row(children: [
+              Text(((data['question_index'] as int) + 1).toString()),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(data['question'] as String),
+                    const SizedBox(height: 5),
+                    Text(data['user_answer'] as String),
+                    Text(data['correct_answer'] as String),
+                  ],
+                ),
+              )
+            ],);
+          },).toList(),
+        ),
+      ),
     );
   }
 }

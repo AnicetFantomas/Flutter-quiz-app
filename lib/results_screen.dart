6261be411f9ac8cc177b263a2d1451ee85ebcd3d
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-12 16:29:06 +0200

    Get more summary details

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -26,6 +26,12 @@ class ResultScreen extends StatelessWidget {
 
     @override
     Widget build(context) {
+      final summaryData = getSummaryData();
+      final numTotalQuestions = questions.length;
+      final numCorrectQuestions = summaryData.where((data) {
+        return data['user_answer'] == data['correct_answer'];
+      }).length;
+
       return SizedBox(
         width: double.infinity,
         child: Container(
@@ -33,11 +39,11 @@ class ResultScreen extends StatelessWidget {
           child:  Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [ 
-             const Text(
-                'You answered X out of Y questions correctly!',
+              Text(
+                'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
               ),
             const  SizedBox(height: 30),
-              QuestionSummary(getSummaryData()),
+              QuestionSummary(summaryData),
               const SizedBox(height: 30),
               TextButton(onPressed: () {}, child: const Text('Restart Quiz'))
             ],
